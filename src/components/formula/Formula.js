540011a628333dca2472f6a61ae5655345314cfc
Vladimir Sidorovsky <wsidorowskiy@gmail.com>
2022-10-13 12:08:15 +0300

    Finish base Framework functional

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,18 @@
+import {ExcelComponent} from '@core/ExcelComponent'
+
+export class Formula extends ExcelComponent {
+  static className = 'excel__formula'
+
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click']
+    })
+  }
+
+  onInput(event) {
+    console.log('Formula onInput', event)
+  }
+
+  onClick() {}
+}
