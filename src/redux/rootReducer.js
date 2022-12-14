6eb461bbdc0e243adfd5b6309e1b81647f10d263
Vladimir Sidorovsky <wsidorowskiy@gmail.com>
2022-12-14 16:39:14 +0200

    Routing implemented

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,7 +3,8 @@ import {
   CHANGE_TEXT,
   CHANGE_STYLES,
   CHANGE_TITLE,
-  TABLE_RESIZE
+  TABLE_RESIZE,
+  UPDATE_DATE
 } from '@/redux/types'
 
 export function rootReducer(state, action) {
@@ -35,6 +36,8 @@ export function rootReducer(state, action) {
       }
     case CHANGE_TITLE:
       return {...state, title: action.data}
+    case UPDATE_DATE:
+      return {...state, openedDate: new Date().toJSON()}
     default: return state
   }
 }
